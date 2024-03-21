d9a247fc1414a009ac5d620601d4ffa96a216161
Duy Thai <duyhthai@gmail.com>
2024-03-21 21:57:09 +0700

    Read text in images with Azure AI Vision

--- Labfiles/05-ocr/C-Sharp/read-text/Program.cs
+++ Labfiles/05-ocr/C-Sharp/read-text/Program.cs
@@ -1,21 +1,17 @@
 using System;
+using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Drawing;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Azure;
+using Microsoft.Extensions.Configuration;
 
 // Import namespaces
-
+using Azure.AI.Vision.ImageAnalysis;
 
 namespace read_text
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             try
@@ -27,7 +23,9 @@ namespace read_text
                 string aiSvcKey = configuration["AIServicesKey"];
 
                 // Authenticate Azure AI Vision client
-
+                ImageAnalysisClient client = new ImageAnalysisClient(
+                    new Uri(aiSvcEndpoint),
+                    new AzureKeyCredential(aiSvcKey));
 
                 // Menu for text reading functions
                 Console.WriteLine("\n1: Use Read API for image (Lincoln.jpg)\n2: Read handwriting (Note.jpg)\nAny other key to quit\n");
@@ -61,12 +59,58 @@ namespace read_text
             Console.WriteLine($"\nReading text from {imageFile} \n");
 
             // Use a file stream to pass the image data to the analyze call
-            using FileStream stream = new FileStream(imageFile,
-                                                     FileMode.Open);
+            using FileStream stream = new FileStream(imageFile, FileMode.Open);
 
             // Use Analyze image function to read text in image
-            
-    
+            ImageAnalysisResult result = client.Analyze(
+                BinaryData.FromStream(stream),
+                // Specify the features to be retrieved
+                VisualFeatures.Read);
+
+            stream.Close();
+
+            // Display analysis results
+            if (result.Read != null)
+            {
+                Console.WriteLine($"Text:");
+
+                // Prepare image for drawing
+                System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
+                Graphics graphics = Graphics.FromImage(image);
+                Pen pen = new Pen(Color.Cyan, 3);
+
+                foreach (var line in result.Read.Blocks.SelectMany(block => block.Lines))
+                {
+                    // Return the text detected in the image
+                    Console.WriteLine($"   '{line.Text}'");
+
+                    // Draw bounding box around line
+                    var drawLinePolygon = true;
+
+                    // Return each line detected in the image and the position bounding box around each line
+                    // Return each word detected in the image and the position bounding box around each word with the confidence level of each word
+
+                    // Draw line bounding polygon
+                    if (drawLinePolygon)
+                    {
+                        var r = line.BoundingPolygon;
+
+                        Point[] polygonPoints = {
+                            new Point(r[0].X, r[0].Y),
+                            new Point(r[1].X, r[1].Y),
+                            new Point(r[2].X, r[2].Y),
+                            new Point(r[3].X, r[3].Y)
+                        };
+
+                        graphics.DrawPolygon(pen, polygonPoints);
+                    }
+                }
+
+                // Save image
+                String output_file = "text.jpg";
+                image.Save(output_file);
+                Console.WriteLine("\nResults saved in " + output_file + "\n");
+            }
         }
     }
 }
