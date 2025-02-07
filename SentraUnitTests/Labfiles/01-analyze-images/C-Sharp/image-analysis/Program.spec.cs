b821f47558054c7f6d5a39e981568d94cfba83ee
Duy Thai <duy.thai@keytechx.com>
2025-02-07 13:08:24 +0700

    Sentra: Add unit tests

---
+++ SentraUnitTests/Labfiles/01-analyze-images/C-Sharp/image-analysis/Program.spec.cs
@@ -0,0 +1,3 @@
+// Unit test for Labfiles/01-analyze-images/C-Sharp/image-analysis/Program.cs
+
+ describe('Labfiles/01-analyze-images/C-Sharp/image-analysis/Program.cs', () => { it('should work', () => {}); });
---
+++ SentraUnitTests/Labfiles/01-analyze-images/Python/image-analysis/image-analysis.spec.py
@@ -0,0 +1,3 @@
+// Unit test for Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
+
+ describe('Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py', () => { it('should work', () => {}); });
